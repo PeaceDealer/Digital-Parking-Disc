8d18b5343611b4dcf13119d186a0d2a7b6bbdc72
PeaceDealer <me@peacedealer.xyz>
2021-04-21 14:22:19 +0100

    working on stuff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from PIL import Image, ImageFont, ImageDraw
+from PIL import Image, ImageFont, ImageDraw, ImageOps
+from inky import InkyWHAT
 import math
 import time
 
@@ -8,7 +9,7 @@ def clockhand(angle, length):
    Angle 0 is 12 o'clock, 90 is 3 o'clock.
    Based around (32,32) as origin, (0,0) in top left.
    """
-   offset = 100
+   offset = 150
 
    radian_angle = math.pi * angle / 180.0
    x = offset + length * math.cos(radian_angle)
@@ -21,11 +22,11 @@ def tick(angle, length):
    Angle 0 is 12 o'clock, 90 is 3 o'clock.
    Based around (32,32) as origin, (0,0) in top left.
    """
-   offset = 100
+   offset = 150
 
    radian_angle = math.pi * angle / 180.0
-   x = int(offset + 75 * math.cos(radian_angle))
-   y = int(offset + 75 * math.sin(radian_angle))
+   x = int(offset + 120 * math.cos(radian_angle))
+   y = int(offset + 120 * math.sin(radian_angle))
 
    sX = int(x-length * math.cos(radian_angle))
    sY = int(y-length * math.sin(radian_angle))
@@ -33,25 +34,36 @@ def tick(angle, length):
 
    return [(sX,sY),(x,y)]
 
-img = Image.new("RGB", (400, 300), (255, 255, 255))
+
+inky_display = InkyWHAT('red')
+
+colourBackground = inky_display.WHITE
+colourForeground = inky_display.BLACK
+colourSpecial = inky_display.RED
+
+inky_display.set_border(colourBackground)
+
+img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT), colourBackground)
 
 draw = ImageDraw.Draw(img)
-draw.ellipse((20, 20, 180, 180), fill = 'white', outline ='black', width=3)
+draw.ellipse((20, 20, 280, 280), fill = None, outline = colourForeground, width=5)
 
-baseLength = 80
-#draw.line(clockhand(0, baseLength + 0), fill='red', width=3) # Minute hand.
-draw.line(clockhand(90, baseLength + 0), fill='red', width=3) # Minute hand.
+draw.line(clockhand(90, 130), fill=colourSpecial, width=3) # Hand.
 
 for x in range(360):
     if( (x % 30) == 0):
-        draw.line(tick(x, 7), fill='black', width=3) # Hour Ticks
+        draw.line(tick(x, 10), fill=colourForeground, width=5) # Hour Ticks
 
     if((x % 10) == 0 and (x % 30) != 0):
         
-        draw.line(tick(x, 2), fill='black', width=1) # Hour Ticks
+        draw.line(tick(x, 5), fill=colourForeground, width=3) # Ticks
 
 font = ImageFont.truetype(r'RobotoMono-VariableFont_wght.ttf', 40)
-# draw.text((x, y),"Sample Text",(r,g,b))
-draw.text((20, 200),"<P> 6:00", 'black', font=font)
 
-img.save("image.png", "PNG")
+draw = ImageDraw.Draw(img)
+draw.text((220, 20),"P 6:00", colourForeground, font=font)
+
+inky_display.set_image(img)
+inky_display.show()
+
+img.save("image.png")
