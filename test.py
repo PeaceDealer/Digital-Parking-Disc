d1f7eee13096a40c42886eeb259500626dce0d1c
PeaceDealer <me@peacedealer.xyz>
2021-04-21 20:44:40 +0100

    fixed image save issue

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ draw = ImageDraw.Draw(image)
 
 draw.ellipse((20, 20, 280, 280), fill=None, outline=1, width=5)
 
-image.putpalette([255, 255, 255, 0, 0, 0])
+#image.putpalette([255, 255, 255, 0, 0, 0])
 
 image = image.resize((600, 600), Image.NEAREST)
 
